9ccb0ade189ae19edef1c6b66895e4ea7f55fd1a
Joshua <joshuadelgado073@gmail.com>
2026-08-11 03:07:02 +0800

    Configure Axios for local and production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://kinewrite-backend.onrender.com/api"
+      : "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
